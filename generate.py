e5db94bcdb0e2a037f5cfd9e4ae379ac3686b409
Francis Tseng <f+accounts@frnsys.com>
2017-05-14 23:25:02 -0400

    refactored/cleaned up generation script

--- generate.py
+++ generate.py
@@ -8,6 +8,7 @@ from PIL import Image, ImageDraw
 from face_sim import compare
 from itertools import chain
 from pack import pack
+from similarity import compute_dist, compute_dists
 
 SAMPLE = 200
 SIZE = (1400, 800)
@@ -16,140 +17,156 @@ MAX_SIZE = (0.5, 0.5)
 MAX_SIZE = (MAX_SIZE[0]*SIZE[0], MAX_SIZE[1]*SIZE[1])
 images = random.sample(glob('../reality/data/_images/*'), SAMPLE)
 
+def noise(strength):
+    return (random.random() - 0.5) * strength
+
 
 def get_objects(impath):
-    imhash = impath.split('/')[-1]
-    dir = 'data/objects/{}'.format(imhash)
+    imid = impath.split('/')[-1]
+    dir = 'data/objects/{}'.format(imid)
     try:
         bboxes = json.load(open('{}/bboxes.json'.format(dir), 'r'))
         labels = json.load(open('{}/labels.json'.format(dir), 'r'))
         crops = glob('{}/*.jpg'.format(dir))
-        return crops, bboxes, labels
+        return [{
+            'path': c,
+            'bbox': b,
+            'label': l,
+            'imid': imid
+        } for c, b, l in zip(crops, bboxes, labels)]
     except FileNotFoundError:
-        return [], [], []
+        return []
 
 
 def get_faces(impath):
-    imhash = impath.split('/')[-1]
-    dir = 'data/faces/{}'.format(imhash)
+    imid = impath.split('/')[-1]
+    dir = 'data/faces/{}'.format(imid)
     try:
         bboxes = json.load(open('{}/bboxes.json'.format(dir), 'r'))
         crops = glob('{}/*.jpg'.format(dir))
-        return crops, bboxes
+        return [{
+            'path': c,
+            'bbox': b,
+            'imid': imid
+        } for c, b in zip(crops, bboxes)]
     except FileNotFoundError:
-        return [], []
+        return []
 
 
-def get_imhash(path):
-    parts = path.split('/')
-    imhash = parts[-2]
-    id = parts[-1].split('.')[0]
-    return imhash, id
+def get_impath(imid):
+    return '../reality/data/_images/{}'.format(imid)
+
+def im_dist(imid_a, imid_b):
+    ims = [Image.open(get_impath(im)) for im in [imid_a, imid_b]]
+    return compute_dist(*ims)
 
 
-def get_impath(imhash):
-    return '../reality/data/_images/{}'.format(imhash)
+def filter_pairs(pairs, images):
+    # remove permutations
+    pairs = set(tuple(sorted(p)) for p in pairs)
+
+    # filter out self-pairings
+    # filter out pairs that are between two of the same images
+    # filter out pairs that are from really similar images
+    pairs = [(a, b) for a, b in pairs
+             if a != b
+             and images[a]['imid'] != images[b]['imid']
+             and im_dist(images[a]['imid'], images[b]['imid']) > 0.5] # TODO adjust this thresh
+    return pairs
 
 
-all_faces = []
-all_fbboxes = []
-FACE_DIST_THRESH = 0.4
-for impath in images:
-    fpaths, fbboxes = get_faces(impath)
-    opaths, obboxes, olabels = get_objects(impath)
-    all_faces.extend(fpaths)
-    all_fbboxes.extend(fbboxes)
+def get_similar_faces(faces, thresh):
+    dists = compare([f['path'] for f in faces])
+    pairs = np.argwhere(dists <= thresh)
+    pairs = filter_pairs(pairs, faces)
 
-print('faces:', len(all_faces))
+    # get unique dist mat indices
+    indices = set(chain(*pairs))
+    return pairs, {idx: faces[idx] for idx in indices}
 
-dists = compare(all_faces)
-idx = np.argwhere(dists <= FACE_DIST_THRESH)
 
-# filter out self-pairings
-idx = [(a, b) for a, b in idx if a != b]
+def get_similar_objects(objs, thresh):
+    dists = compute_dists([o['path'] for o in objs])
+    pairs = np.argwhere(dists <= thresh)
+    pairs = filter_pairs(pairs, objs)
 
-# remove permutations
-idx = set(tuple(sorted(l)) for l in idx)
+    # get unique dist mat indices
+    indices = set(chain(*pairs))
+    return pairs, {idx: objs[idx] for idx in indices}
 
-# get unique dist mat indices
-imdx = set(chain(*idx))
 
-# create lookup for dist mat indices
-# to image name hash
-hash_idx = {}
-for i in imdx:
-    path = all_faces[i]
-    imhash, id = get_imhash(path)
-    hash_idx[i] = imhash
+def scale_rect(rect, scale):
+    return (rect[0] * scale[0], rect[1] * scale[1],
+            rect[2] * scale[0], rect[3] * scale[1])
 
-# filter out pairs that are between two of the same images
-idx = [(a, b) for a, b in idx if hash_idx[a] != hash_idx[b]]
 
-canvas = Image.new('RGB', SIZE, color=0)
-draw = ImageDraw.Draw(canvas)
-if idx:
-    pair = random.choice(idx)
-    imhashes = [(hash_idx[matidx], matidx) for matidx in pair]
-    preppedims = []
-    preppedbboxes = []
-    matidxs = []
-    # prepare images
-    bbox_lookup = {}
-    for imhash, matidx in imhashes:
-        im = Image.open(get_impath(imhash))
+def shift_rect(rect, shift):
+    return (rect[0] + shift[0], rect[1] + shift[1],
+            rect[2] + shift[0], rect[3] + shift[1])
+
+
+def prepare_images(images):
+    for image in images.values():
+        im = Image.open(get_impath(image['imid']))
         xo, yo = im.size
+
         im = immanip.resize_to_limit(im, MAX_SIZE)
         xn, yn = im.size
+
         resize_ratio = (xn/xo, yn/yo)
 
         # scale bbox accordingly
-        bbox = all_fbboxes[matidx]
-        bbox = (
-            (bbox[0] * resize_ratio[0]), (bbox[1] * resize_ratio[1]),
-            (bbox[2] * resize_ratio[0]), (bbox[3] * resize_ratio[1]))
+        image['bbox'] = scale_rect(image['bbox'], resize_ratio)
+        image['image'] = im
+    return images
 
-        preppedims.append(im)
-        preppedbboxes.append(bbox)
-        matidxs.append(matidx)
 
+def render(images, pairs, out='output.jpg'):
+    canvas = Image.new('RGB', SIZE, color=0)
+    draw = ImageDraw.Draw(canvas)
+
+    # layout images
     # this doesn't guarantee that every image will be included!
-    packed = pack(preppedims, canvas)
-    for im, pos, bbox, matidx in zip(preppedims, packed, preppedbboxes, matidxs):
+    packed = pack([im['image'] for im in images.values()], canvas)
+    for im, pos in zip(images.values(), packed):
         pos = (
-            round(pos[0] + (random.random() - 0.5) * SHAKINESS),
-            round(pos[1] + (random.random() - 0.5) * SHAKINESS))
-        canvas.paste(im, pos)
-        bbox = (
-            bbox[0] + pos[0], bbox[1] + pos[1],
-            bbox[2] + pos[0], bbox[3] + pos[1])
-        bbox_lookup[matidx] = bbox
-        annotate.circle(draw, bbox)
+            round(pos[0] + noise(SHAKINESS)),
+            round(pos[1] + noise(SHAKINESS)))
+        canvas.paste(im['image'], pos)
+        im['bbox'] = shift_rect(im['bbox'], pos)
+
+    # draw circles
+    for im in images.values():
+        annotate.circle(draw, im['bbox'])
 
     # draw links
-    for a, b in idx:
-        try:
-            bbox_a, bbox_b = bbox_lookup[a], bbox_lookup[b]
-            annotate.link(
-                draw,
-                (bbox_a[0], bbox_a[1]),
-                (bbox_b[0], bbox_b[1]))
-        except KeyError:
-            pass
+    for a, b in pairs:
+        annotate.link(draw, images[a]['bbox'][:2], images[b]['bbox'][:2])
+    canvas.save(out)
 
+if __name__ == '__main__':
+    FACE_DIST_THRESH = 0.4
 
-    canvas.save('output.jpg')
-else:
-    print('nothing!')
+    faces = []
+    objects = []
+    for path in images:
+        faces.extend(get_faces(path))
+        objects.extend(get_objects(path))
 
+    print('faces:', len(faces))
+    print('objects:', len(objects))
+    fpairs, faces = get_similar_faces(faces, FACE_DIST_THRESH)
+    opairs, objects = get_similar_objects(objects, FACE_DIST_THRESH)
 
+    # since indices will collide, add prefixes
+    fpairs = [('f{}'.format(a), 'f{}'.format(b)) for a, b in fpairs]
+    faces = {'f{}'.format(k): v for k, v in faces.items()}
+    opairs = [('o{}'.format(a), 'o{}'.format(b)) for a, b in opairs]
+    objects = {'o{}'.format(k): v for k, v in objects.items()}
+    pairs = fpairs + opairs
 
-# im = Image.open(impath)
-
-# for bbox, (label, confidence) in zip(obj_bboxes, obj_labels):
-#     if confidence > 0.3:
-#         annotate.circle(draw, bbox)
-
-# for bbox in face_bboxes:
-#     annotate.circle(draw, bbox)
-
-# im.save('output.jpg')
+    if pairs:
+        images = prepare_images(objects)
+        render(images, opairs, out='output.jpg')
+        # images = prepare_images({**faces, **objects})
+        # render(images, fpairs + opairs, out='output.jpg')
--- similarity.py
+++ similarity.py
@@ -3,21 +3,20 @@ import numpy as np
 from PIL import Image
 from itertools import combinations
 
-
-def compute_hashes(files, hashfunc=imagehash.whash):
-    hashes, fnames = [], []
-    for i, fname in enumerate(files):
-        img = Image.open(fname)
-        hash = hashfunc(img)
-        hashes.append(hash)
-        fnames.append(fname)
-    return hashes, fnames
+hashfunc = imagehash.whash
 
 
-def compute_dists(hashes):
+def compute_dists(paths):
     # precompute distance matrix
-    mat = np.zeros((len(hashes), len(hashes)))
+    mat = np.zeros((len(paths), len(paths)))
+    hashes = [hashfunc(Image.open(p)) for p in paths]
     for i, j in combinations(range(len(hashes)), 2):
         dist = hashes[i] - hashes[j]
         mat[i, j] = mat[j,i] = dist
     return mat
+
+
+def compute_dist(img_a, img_b):
+    hash_a = hashfunc(img_a)
+    hash_b = hashfunc(img_b)
+    return hash_a - hash_b
